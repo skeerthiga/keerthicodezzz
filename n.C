3870294a0e01902fe148c3b697c3c5406560fa85
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 13:53:06 +0530

    Update n.C

--- n.C
+++ n.C
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
 {
-int n, i, sum = 0;
+int n,i,sum=0;
 printf("Enter a positive integer");
 scanf("%d",&n);
-for(i=1; i <= n; ++i)
+i = 1;
+while ( i <=n )
 {
 sum += i;
+++i;
 }
 printf("Sum = %d",sum);
 return 0;
