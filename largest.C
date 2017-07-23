e4a48846baaa66562f58d1050c9cf86962e760bb
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 10:25:16 +0530

    Create largest.C

---
+++ largest.C
@@ -0,0 +1,23 @@
+#inlude<stdio.h>
+void main()
+{
+int j1, j2, j3;
+printf("Enter the values of num1, num2 and num3\n");
+scanf("%d %d %d", &j1, &j2, &j3);
+printf("j1 = %d\tj2 = %d\tj3 = %d\n",j1,j2,j3);
+if (j1 > j2)
+{
+if (j1 >j3)
+{
+printf("j1 is the greatest among three \n");
+}
+els
+{
+printf("j3 is the greatest among three \n");
+}
+}
+else if (num2 > num3)
+printf("num2 is the greatest among three \n");
+else
+printf("num3 is the greatest among three \n");
+}
