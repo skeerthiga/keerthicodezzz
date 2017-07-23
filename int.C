604bdccd13b6e1775e641deb4e893944e823e214
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 15:59:33 +0530

    Create int.C

---
+++ int.C
@@ -0,0 +1,16 @@
+# include <stdio.h> 
+# include <conio.h> 
+void main() 
+{ 
+int n, count = 0 ; 
+clrscr() ; 
+printf("Enter a number: ") ; 
+scanf("%d", &n) ; 
+while(n > 0) 
+{ 
+count++ ; 
+n = n/10 ; 
+} 
+printf("\nThe number of digits is: %d", count) ; 
+getch() ; 
+}
