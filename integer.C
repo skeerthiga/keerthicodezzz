1097518a26bd57b38c5deee4e75e3edf8a518aa6
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 14:50:11 +0530

    Update integer.C

--- integer.C
+++ integer.C
@@ -3,12 +3,11 @@ int main()
 {
 long long n;
 int count = 0;
-printf("Enter an integer: ");
+printf("Enter an integer");
 scanf("%lld", &n);
-while(n != 0)
+while(n!=0)
 {
-n = n/10
 n /= 10;
 ++count;
-printf("Number of digits: %d", count);
+printf("Number of digits: %d",count);
 }
