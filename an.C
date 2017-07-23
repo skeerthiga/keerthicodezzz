742962d21448549bd0347c4793f22639b3455fce
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 10:06:02 +0530

    Create an.C

---
+++ an.C
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <ctype.h> 
+int main() {  
+char ch;
+printf("Enter a Ch\n");  
+scanf("%c", &ch);  
+if(isalpha(character)) {  
+printf("%c is an Alphabet\n", ch);  
+} 
+else 
+{  
+printf("%c is Not an Alphabet\n", ch);  
+}
+return 0;  
+}
