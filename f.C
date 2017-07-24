90dcf4bd98427164b939852f19c529a2235b4d75
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-24 16:02:12 +0530

    Update f.C

--- f.C
+++ f.C
@@ -1,17 +1,17 @@
 #include <stdio.h>
 void main()
 {
-int i,fact = 1,num;
-printf("Enter the number \n");
+int o,fact=1,num;
+printf("Enter the number\n");
 scanf("%d", &num);
 if(num<=0)
 fact=1;
 else
 {
-for(i=1;i<=num;i++)
+for(o=1;o<=num;o++)
 {
-fact=fact*i;
+fact=fact*o;
 }
 }
-  printf("Factorial of %d = %5d\n",num,fact);
+  printf("Factorial of %d=%5d\n",num,fact);
 }
