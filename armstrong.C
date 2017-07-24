d03e998362b7744cd0a2383e2c38095162626fa8
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-24 12:14:05 +0530

    Create armstrong.C

---
+++ armstrong.C
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+int num,l,sum=0,t;
+printf("Enter a number: ");
+scanf("%d",&num);
+t=num;
+while(num!=0)
+{
+r=num%10;
+num=num/10;
+sum=sum+(l*l*l);
+}
+if(sum==temp)
+printf("%d is an Armstrong number",t);
+else
+printf("%d is not an Armstrong number",t);
+return 0;
+}
