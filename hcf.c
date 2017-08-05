a7de3dfeb4a776411f8a769812d7fb1a684068f4
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-08-05 16:31:36 +0530

    Create hcf.c

---
+++ hcf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int gcd(int j,int k)
+{
+if (j==0||k==0)
+return 0;
+if (j==k)
+return j;
+if (j>k)
+return gcd(j-k,k);
+return gcd(j,j-k);
+}
+int main()
+{
+int j=98,k=56;
+printf("GCD of %d and %d is %d ", j,k,gcd(j,k));
+return 0;
+}
